b9a9f1a687d713236559939f2a87618bbf54decc
sagarsv <sagar.vishwakarma@decimalpointanalytics.com>
2018-04-18 19:50:09 +0530

    added methods to seperate data

--- ImageClassifier/README.md
+++ ImageClassifier/README.md
@@ -5,6 +5,15 @@ The code uses Keras & Tensorflow to train a convolutional neural network on a la
 By changing the model architecture it can be used to classify multiple labels.
 
 
+Download Data from : https://www.kaggle.com/c/dogs-vs-cats/data
+For now split the "train" data into "train" and "validation". Ratio 80:20
+Also keep the in directories.
+  train cats data into "/data/train/cat/"
+  train dogs data into "/data/train/dog/"
+  validation cats data into "/data/validation/cat/"
+  validation dogs data into "/data/validation/dog/"
+
+
 Language
 ============
 
--- ImageClassifier/cat_vs_dog.py
+++ ImageClassifier/cat_vs_dog.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import shutil
+from tqdm import tqdm
+import random
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -9,17 +12,20 @@ from keras import optimizers
 
 
 # os.getcwd()
-# os.chdir(r'D:\CodeRepo\DeepLearningIntro\MakeDataAmazing')
+# os.chdir(r'E:\to_be_deleted\DeepLearningIntro\ImageClassifier')
 
+# If you wanna use pre trained model head on to
+# https://github.com/llSourcell/how_to_make_an_image_classifier/blob/master/demo.ipynb
 
 img_size = 150
 
+# The path needs to be to a folder, that contains folders, that contains the images.
 train_data_dir = './data/train/'
 validation_data_dir = './data/validation/'
 
-save_weight = True
+save_weight = False
 
-epoch = 30
+epoch = 10
 train_samples = 2048
 validation_samples = 832
 
@@ -27,15 +33,15 @@ validation_samples = 832
 def create_small_conv_net():
     # Model architecture definition
     model = Sequential()
-    model.add(Convolution2D(32, 3, 3, input_shape=(img_size, img_size,3)))
+    model.add(Convolution2D(32, (3, 3), input_shape=(img_size, img_size,3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Convolution2D(32, 3, 3))
+    model.add(Convolution2D(32, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Convolution2D(64, 3, 3))
+    model.add(Convolution2D(64, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -55,39 +61,39 @@ def create_vgg_16_conv_net():
     # Model architecture definition
     model = Sequential()
     model.add(ZeroPadding2D((1, 1), input_shape=(img_size, img_size,3)))
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+    model.add(Convolution2D(64, (3, 3), activation='relu', name='conv1_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+    model.add(Convolution2D(64, (3, 3), activation='relu', name='conv1_2'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+    model.add(Convolution2D(128, (3, 3), activation='relu', name='conv2_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+    model.add(Convolution2D(128, (3, 3), activation='relu', name='conv2_2'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+    model.add(Convolution2D(256, (3, 3), activation='relu', name='conv3_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+    model.add(Convolution2D(256, (3, 3), activation='relu', name='conv3_2'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+    model.add(Convolution2D(256, (3, 3), activation='relu', name='conv3_3'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+    model.add(Convolution2D(512, (3, 3), activation='relu', name='conv4_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+    model.add(Convolution2D(512, (3, 3), activation='relu', name='conv4_2'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+    model.add(Convolution2D(512, (3, 3), activation='relu', name='conv4_3'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+    model.add(Convolution2D(512, (3, 3), activation='relu', name='conv5_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+    model.add(Convolution2D(512, (3, 3), activation='relu', name='conv5_2'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+    model.add(Convolution2D(512, (3, 3), activation='relu', name='conv5_3'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     # last layer same as our small conv_net
@@ -103,6 +109,51 @@ def create_vgg_16_conv_net():
     return model
 
 
+def split_data_into_cat_dog():
+
+    train_data_list = os.listdir(train_data_dir)
+    move_cat = False
+    move_dog = False
+    if not os.path.exists(train_data_dir+'cat/'):
+        os.makedirs(train_data_dir+'cat/')
+        move_cat = True
+    if not os.path.exists(train_data_dir+'dog/'):
+        os.makedirs(train_data_dir+'dog/')
+        move_dog = True
+    print("Split Dog and Cat")
+    for file in tqdm(train_data_list):
+        # file = train_data_list[1]
+        if 'dog' in file and move_dog:
+            shutil.move(train_data_dir+file, train_data_dir+'dog/'+file)
+        elif 'cat' in file and move_cat:
+            shutil.move(train_data_dir+file, train_data_dir+'cat/'+file)
+        else:
+            pass
+
+    return 0
+
+
+def split_data_into_validation():
+    print("Create validation data")
+    if not os.path.exists(validation_data_dir+'cat/'):
+        os.makedirs(validation_data_dir+'cat/')
+        cat_data_list = os.listdir(train_data_dir+'cat/')
+        # Take random 20% of data
+        cat_data_list = random.sample(cat_data_list, int(len(cat_data_list)*0.2))
+        for file in cat_data_list:
+            shutil.move(train_data_dir+'cat/'+file, validation_data_dir+'cat/'+file)
+
+    if not os.path.exists(validation_data_dir+'dog/'):
+        os.makedirs(validation_data_dir+'dog/')
+        dog_data_list = os.listdir(train_data_dir+'dog/')
+        # Take random 20% of data
+        dog_data_list = random.sample(dog_data_list, int(len(dog_data_list)*0.2))
+        for file in dog_data_list:
+            shutil.move(train_data_dir+'dog/'+file, validation_data_dir+'dog/'+file)
+
+    return True
+
+
 def run_small_conv_net(model, train_generator, validation_generator, augmented):
 
     if not model:
@@ -111,7 +162,7 @@ def run_small_conv_net(model, train_generator, validation_generator, augmented):
     if augmented:
         print("Training model with data augmentation")
 
-    model.fit_generator(train_generator, samples_per_epoch=train_samples, nb_epoch=epoch, validation_data=validation_generator, nb_val_samples=validation_samples)
+    model.fit_generator(train_generator, validation_data=validation_generator, validation_steps=validation_samples, epochs=epoch, steps_per_epoch=train_samples)
 
     if save_weight:
         if augmented:
@@ -137,7 +188,7 @@ def run_vgg_16(model, train_generator, validation_generator, augmented):
     if augmented:
         print("Training vgg model with data augmentation")
 
-    model.fit_generator(train_generator, samples_per_epoch=train_samples, nb_epoch=epoch, validation_data=validation_generator, nb_val_samples=validation_samples)
+    model.fit_generator(train_generator, validation_data=validation_generator, validation_steps=validation_samples, epochs=epoch, steps_per_epoch=train_samples)
 
     if save_weight:
         if augmented:
@@ -158,29 +209,36 @@ def run_vgg_16(model, train_generator, validation_generator, augmented):
 
 def run_classifier(arg):
 
-    # used to rescale the pixel values from [0, 255] to [0, 1] interval
-    datagen = ImageDataGenerator(rescale=1./255)
-    # automagically retrieve images and their classes for train and validation sets
-    train_generator = datagen.flow_from_directory(train_data_dir, target_size=(img_size, img_size), batch_size=32, class_mode='binary')
-    validation_generator = datagen.flow_from_directory(validation_data_dir, target_size=(img_size, img_size), batch_size=32, class_mode='binary')
+    split_data_into_cat_dog()
 
-    # Data augmentation for improving the model
-    datagen_augmented = ImageDataGenerator(
-                                rescale=1./255,        # normalize pixel values to [0,1]
-                                shear_range=0.2,       # randomly applies shearing transformation
-                                zoom_range=0.2,        # randomly applies shearing transformation
-                                horizontal_flip=True)  # randomly flip the images
+    ret = split_data_into_validation()
 
-    train_generator_augmented = datagen_augmented.flow_from_directory(train_data_dir, target_size=(img_width, img_height), batch_size=32, class_mode='binary')
+    if ret:
+        # used to rescale the pixel values from [0, 255] to [0, 1] interval
+        datagen = ImageDataGenerator(rescale=1./255)
+        # automagically retrieve images and their classes for train and validation sets
+        train_generator = datagen.flow_from_directory(train_data_dir, target_size=(img_size, img_size), batch_size=32, class_mode='binary')
+        validation_generator = datagen.flow_from_directory(validation_data_dir, target_size=(img_size, img_size), batch_size=32, class_mode='binary')
 
-    if arg:
-        print("Running Vgg convnet")
-        model_vgg = run_vgg_16(None, train_generator, validation_generator, False)
-        model_vgg = run_vgg_16(model_vgg, train_generator_augmented, validation_generator, True)
+        # Data augmentation for improving the model
+        datagen_augmented = ImageDataGenerator(
+                                    rescale=1./255,        # normalize pixel values to [0,1]
+                                    shear_range=0.2,       # randomly applies shearing transformation
+                                    zoom_range=0.2,        # randomly applies shearing transformation
+                                    horizontal_flip=True)  # randomly flip the images
+
+        train_generator_augmented = datagen_augmented.flow_from_directory(train_data_dir, target_size=(img_size, img_size), batch_size=32, class_mode='binary')
+
+        if arg:
+            print("Running Vgg convnet")
+            model_vgg = run_vgg_16(None, train_generator, validation_generator, False)
+            model_vgg = run_vgg_16(model_vgg, train_generator_augmented, validation_generator, True)
+        else:
+            print("Running small convnet")
+            model = run_small_conv_net(None, train_generator, validation_generator, False)
+            model = run_small_conv_net(model, train_generator_augmented, validation_generator, True)
     else:
-        print("Running small convnet")
-        model = run_small_conv_net(None, train_generator, validation_generator, False)
-        model = run_small_conv_net(model, train_generator_augmented, validation_generator, True)
+        print("Dataset Split Error, Please check!")
 
     return 0
 
@@ -191,7 +249,7 @@ def main():
         arg = sys.argv[1]
         if arg=='1' or arg=='0':
             arg = int(arg)
-            # run_classifier(arg)
+            run_classifier(arg)
         else:
             print("Invalid argumet encountered")
     else:
