c66cf78b41aea97265dc4af927d34069471cd446
sagarsv <sagar.vishwakarma@decimalpointanalytics.com>
2018-04-19 12:28:38 +0530

    added weight and layer ploting

--- ImageClassifier/classify_digits.py
+++ ImageClassifier/classify_digits.py
@@ -78,6 +78,14 @@ def plot_images(images, cls_true, cls_pred=None):
     plt.show()
 
 
+def plot_image(image):
+    plt.imshow(image.reshape(img_shape),
+               interpolation='nearest',
+               cmap='binary')
+
+    plt.show()
+
+
 def plot_few_data(data):
     # Plot a few images to see if data is correct
     # Get the first images from the test-set.
@@ -263,6 +271,97 @@ def plot_confusion_matrix(cls_pred, data):
     plt.show()
 
 
+def plot_conv_weights(session, weights, input_channel=0):
+    # Assume weights are TensorFlow ops for 4-dim variables
+    # e.g. weights_conv_one or weights_conv_two.
+
+    # Retrieve the values of the weight-variables from TensorFlow.
+    # A feed-dict is not necessary because nothing is calculated.
+    w = session.run(weights)
+
+    # Get the lowest and highest values for the weights.
+    # This is used to correct the colour intensity across
+    # the images so they can be compared with each other.
+    w_min = np.min(w)
+    w_max = np.max(w)
+
+    # Number of filters used in the conv. layer.
+    num_filters = w.shape[3]
+
+    # Number of grids to plot.
+    # Rounded-up, square-root of the number of filters.
+    num_grids = math.ceil(math.sqrt(num_filters))
+
+    # Create figure with a grid of sub-plots.
+    fig, axes = plt.subplots(num_grids, num_grids)
+
+    # Plot all the filter-weights.
+    for i, ax in enumerate(axes.flat):
+        # Only plot the valid filter-weights.
+        if i<num_filters:
+            # Get the weights for the i'th filter of the input channel.
+            # See new_conv_layer() for details on the format
+            # of this 4-dim tensor.
+            img = w[:, :, input_channel, i]
+
+            # Plot image.
+            ax.imshow(img, vmin=w_min, vmax=w_max,
+                      interpolation='nearest', cmap='seismic')
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+
+def plot_conv_layer(session, layer, image):
+    # Assume layer is a TensorFlow op that outputs a 4-dim tensor
+    # which is the output of a convolutional layer,
+    # e.g. layer_conv1 or layer_conv2.
+
+    # Create a feed-dict containing just one image.
+    # Note that we don't need to feed y_true because it is
+    # not used in this calculation.
+    feed_dict = {x: [image]}
+
+    # Calculate and retrieve the output values of the layer
+    # when inputting that image.
+    values = session.run(layer, feed_dict=feed_dict)
+
+    # Number of filters used in the conv. layer.
+    num_filters = values.shape[3]
+
+    # Number of grids to plot.
+    # Rounded-up, square-root of the number of filters.
+    num_grids = math.ceil(math.sqrt(num_filters))
+
+    # Create figure with a grid of sub-plots.
+    fig, axes = plt.subplots(num_grids, num_grids)
+
+    # Plot the output images of all the filters.
+    for i, ax in enumerate(axes.flat):
+        # Only plot the images for valid filters.
+        if i<num_filters:
+            # Get the output image of using the i'th filter.
+            # See new_conv_layer() for details on the format
+            # of this 4-dim tensor.
+            img = values[0, :, :, i]
+
+            # Plot image.
+            ax.imshow(img, interpolation='nearest', cmap='binary')
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+
 def run_convnet(data):
 
     # Here None means that the tensor may hold an arbitrary number of images with each image being a vector of length img_size_flat(img_size*img_size).
@@ -402,6 +501,19 @@ def run_convnet(data):
         print("Confusion Matrix:")
         plot_confusion_matrix(cls_pred, data)
 
+    # Example image to display through different layers
+    image_one = data.test.images[0]
+    print("First image from test data:")
+    plot_image(image_one)
+    print("First Convnet Weights:")
+    plot_conv_weights(session, weights_conv_one)
+    print("First Convnet Layers for image_one:")
+    plot_conv_layer(session, layer_conv_one, image_one)
+    print("Second Convnet Weights:")
+    plot_conv_weights(session, weights_conv_two)
+    print("Second Convnet Layers for image_one:")
+    plot_conv_layer(session, layer_conv_two, image_one)
+    
     session.close()
 
     return 0
